b3cde9b37affcb7bf0354b531b8bee323b6fcdbb
Xiaofeng Wang <wasphin@gmail.com>
2020-02-10 16:24:10 +0800

    Install dependencies: htmlize

--- publish.el
+++ publish.el
@@ -1,3 +1,12 @@
+(package-initialize)
+(unless package-archive-contents
+  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (package-refresh-contents))
+(dolist (pkg '(htmlize))
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
+
 (require 'org)
 (require 'ox-publish)
 
